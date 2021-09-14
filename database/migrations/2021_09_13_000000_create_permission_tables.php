576d6ba5d8fab61f08293f30262723c5d9e4cfc1
klc <m_kilic@outlook.com.tr>
2021-09-14 13:58:35 +0300

    fixed migration

--- database/migrations/2021_09_13_000000_create_permission_tables.php
+++ database/migrations/2021_09_13_000000_create_permission_tables.php
@@ -26,14 +26,14 @@ class CreatePermissionTables extends Migration
             $table->unique('slug');
         });
 
-        Schema::table('role_permission', function (Blueprint $table) {
+        Schema::create('role_permission', function (Blueprint $table) {
             $table->integer('role_id');
             $table->integer('permission_id');
 
             $table->unique(['role_id', 'permission_id'], 'rp');
         });
 
-        Schema::table('user_role', function (Blueprint $table) {
+        Schema::create('user_role', function (Blueprint $table) {
             $table->integer('user_id');
             $table->integer('role_id');
 
